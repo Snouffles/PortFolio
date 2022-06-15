41ca4d6577c63931f5e19ec89b23e1124bce3936
Donovan Payan <donovan.payan@gmail.com>
2022-06-15 09:19:49 +0200

    better version

--- weatherapp/backend/middlewares/rateLimiter.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require("express-rate-limit");
-
-
-const limiter = rateLimit({
-    windowMS: 10 * 60 * 6000,//10minutes
-    max: 20
-})
-
-module.exports = limiter; 
--- weatherapp/backend/routes/route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const ctrl = require("../controllers/controller");
-const cache = require ("../middlewares/cache.js")
-
-router.get("/",cache("2 minutes"), ctrl.callAPI);
-
-module.exports = router;
